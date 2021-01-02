f29fdc25be3a7ed860fea176a4a8f27aa9a868e2
chiragmahawar7 <cmahawar_be16@thapar.edu>
2021-01-02 20:02:15 +0530

    nav bar & routes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,13 @@ import { Hero } from '../hero';
 import { HeroService } from '../services/hero.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class DashboardComponent implements OnInit {
 
-  heroes: Hero[];
+  heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
 
@@ -19,7 +19,7 @@ export class HeroesComponent implements OnInit {
 
   getHeroes() {
     this.heroService.getHeroes().subscribe(heroes => {
-      this.heroes = heroes;
+      this.heroes = heroes.slice(1, 5)
     });
   }
 }
